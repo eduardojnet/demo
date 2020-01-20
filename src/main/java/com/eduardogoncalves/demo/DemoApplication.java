89a4084b5304dad4d5aefd4a6eccc5cdb0c194f7
Eduardo Goncalves <eduardojnet@gmail.com>
2020-01-20 14:07:01 -0300

    Produto e associação muitos pra muitos

--- src/main/java/com/eduardogoncalves/demo/DemoApplication.java
+++ src/main/java/com/eduardogoncalves/demo/DemoApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.eduardogoncalves.demo.domain.Categoria;
+import com.eduardogoncalves.demo.domain.Produto;
 import com.eduardogoncalves.demo.repositories.CategoriaRepository;
+import com.eduardogoncalves.demo.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner{
@@ -16,10 +18,14 @@ public class DemoApplication implements CommandLineRunner{
 	@Autowired	
 	private CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	// Operação de Intanciação, criando o objeto Categoria
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
@@ -27,7 +33,23 @@ public class DemoApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria( null, "Informatica3");
 		Categoria cat2 = new Categoria( null, "Escritorio3");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Computador", 2000.00);
+		Produto p3 = new Produto(null, "Computador", 2000.00);
+		
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		
 	}
--- src/main/java/com/eduardogoncalves/demo/domain/Categoria.java
+++ src/main/java/com/eduardogoncalves/demo/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.eduardogoncalves.demo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -16,6 +19,9 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy="categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
 		
@@ -47,7 +53,18 @@ public class Categoria implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
 
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
+	
 
 	@Override
 	public int hashCode() {
@@ -74,10 +91,6 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
-
-
-	
-	
 	
 	
 }
